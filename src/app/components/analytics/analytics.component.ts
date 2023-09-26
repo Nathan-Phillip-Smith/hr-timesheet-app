b4b9344f1a9c2b02cd2e5aa2792f459b8fe24863
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-09-26 12:37:45 -0600

    reset at unit 7

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+  styleUrls: ['./analytics.component.scss'],
 })
-export class AnalyticsComponent {
+export class AnalyticsComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
