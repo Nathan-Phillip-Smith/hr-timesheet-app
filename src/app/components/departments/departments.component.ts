dabff4e28073207512decd29b244d35a9cfcd73f
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-09-26 12:04:33 -0600

    unit 5

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrls: ['./departments.component.scss']
+  styleUrls: ['./departments.component.scss'],
 })
-export class DepartmentsComponent {
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] = [];
 
+  constructor(private departmentsService: DepartmentsService) {}
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+  }
 }
